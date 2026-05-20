25424f4967e73a96deca27d3f74994241974815d
Code-By-Amit <amitsaini95471@gmail.com>
2026-05-20 13:29:31 +0530

    fix

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors({
-    origin: [process.env.CLIENT_URL!, "http://localhost:5173"],
+    origin: [process.env.CLIENT_URL!, "https://gig-flow-nu-topaz.vercel.app/"],
     credentials: true
 }));
 
