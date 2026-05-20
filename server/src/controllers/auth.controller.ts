695cd374adb4c1913c179bd13f400e64b8f2c3da
Code-By-Amit <amitsaini95471@gmail.com>
2026-05-20 13:47:26 +0530

    fix

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -71,7 +71,7 @@ export const login = asyncHandler(
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
     })
 
     return res.status(200).json(
